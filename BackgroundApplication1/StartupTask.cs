4719e740b8cfda647f13058042b080e69a806eb3
浜口健一郎 <khamaguchi@bemap.co.jp>
2018-07-02 11:12:20 +0900

    feat: get acceloration in background

--- BackgroundApplication1/StartupTask.cs
+++ BackgroundApplication1/StartupTask.cs
@@ -4,22 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
-
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using System.Diagnostics;
 
 namespace BackgroundApplication1
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private BackgroundTaskDeferral deferral;
+        private Accelerometer accelerometer;
+        private int count;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            deferral = taskInstance.GetDeferral();
+            accelerometer = Accelerometer.GetDefault();
+            if (accelerometer != null)
+            {
+                uint minReportInterval = accelerometer.MinimumReportInterval;
+                Debug.WriteLine($"MinimumReportInterval={minReportInterval}");
+                uint reportInterval = minReportInterval > 16 ? minReportInterval : 16;
+                accelerometer.ReportInterval = reportInterval;
+                accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);
+            }
+        }
+
+        private void ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+        {
+            AccelerometerReading reading = args.Reading;
+            Debug.WriteLine($"x={reading.AccelerationX,5:0.00}, y={reading.AccelerationY,5:0.00}, z={reading.AccelerationZ,5:0.00}");
+            count++;
+            if (count == 20)
+            {
+                // Stop reporting
+                accelerometer.ReportInterval = 0;
+                accelerometer.ReadingChanged -= new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(ReadingChanged);
+            }
         }
     }
 }
